5e33f254d8e9c452f9be1fd822f9c59342afa10b
Izabella Almeida <99206817+1zabella@users.noreply.github.com>
2022-05-04 13:41:34 -0300

    Add files via upload

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/aula 03/aula.js
@@ -0,0 +1,15 @@
+
+function contaVelocidade(){
+    // debugger
+    const gravidade = 10;
+    var VelocidadeInicial = document.getElementById("VelocidadaInicial").value;
+    
+
+    var altura = (VelocidadeInicial**2)/(2*gravidade);
+
+    var tempo = VelocidadeInicial/gravidade;
+
+    console.log(altura , tempo);
+    var ResultadoTela = document.getElementById("ResultadoTela").innerHTML = "<p class='texto'>Altura máxima: " + altura + "<br>"+ "<br>" + " Tempo total: " + tempo +"</p>";
+}
+
