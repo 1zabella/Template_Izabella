77d3776baede552706957f2d9137b1037928ae8b
1zabella <99206817+1zabella@users.noreply.github.com>
2022-05-05 14:32:23 -0300

    changes

--- Docs/Estudo/aula 04/aula-04.js
+++ Docs/Estudo/aula 04/aula-04.js
@@ -1,7 +1,16 @@
 function troca_destroca(){
+    // Atribuição de valores que estão no html.
     var numero1 = document.getElementById("numero1").value; 
     var numero2 = document.getElementById("numero2").value;
-    console.log("teste")
 
+    //Troca dos valores anteriores de forma infinita (troca e desctroca sempre que clica no botão).
+    document.getElementById("numero1").value = numero2
+    document.getElementById("numero2").value = numero1
 
+    // var numero1_original = numero1
+    // var numero1_trocado = (numero1=numero2)
+    // var numero2_trocado = (numero2=numero1_original)
+
+    console.log(numero1_trocado , numero2_trocado)
+    var resultado = document.getElementById("Resultado").innerHTML = "<p class='texto'> Números trocados: <br> Novo número 1: " + numero1_trocado + "<br>" + "Novo número 2: "+ numero2_trocado + "</p>";
 }
